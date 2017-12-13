c571e03b774e6ba8a7f3fb918022bd823c542b97
fresh5447 <doug@kosmojo.com>
2017-12-12 20:17:31 -0700

    fix bug

--- server/routes/heroes.js
+++ server/routes/heroes.js
@@ -1,42 +1,53 @@
 const express = require('express')
 const Router = express.Router()
-const SuperHero = ('../models/SuperHero')
-
-
+const SuperHero = require('../models/SuperHero')
 
 Router.route('/api/superheroes')
-.get((req, res) => {
-  console.log('route found')
-  SuperHero.find((err, superHeroes) => {
-    if (err) {
-      res.json({ error: err })
-    } else {
-      res.json({ msg: 'SUCCESS', results: superHeroes })
-    }
+  .get((req, res) => {
+    SuperHero.find((err, superHeroes) => {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        res.json({ msg: 'SUCCESS', results: superHeroes })
+      }
+    })
   })
-})
 
-  // Router.route('/api/superheroes')
-  // .post((req, res) => {
-  //   const {name, universe, superPower, img, nemesis} = req.body
-  //   const newSuperHero = {
-  //     name,
-  //     universe,
-  //     superPower,
-  //     nemesis,
-  //     img
-  //   }
-  
-  //   SuperHero(newSuperHero).save((err, hero) => {
-  //     if (err) {
-  //       res.json({ msg: err })
-  //     } else {
-  //       res.json({ msg: 'Success', data: hero })
-  //     }
-  //   })
-  // })
+Router.route('/api/superheroes')
+  .post((req, res) => {
+    const {name, superPower, img, universe, nemesis} = req.body
+    const newHero = {name, superPower, img, universe, nemesis}
+    SuperHero(newHero).save((err, savedHero) => {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        res.json({ msg: 'SUCCESS', data: savedHero })
+      }
+    })
+  })
 
- 
+Router.route('/api/superheroes/:heroId')
+  .get((req, res) => {
+    const heroId = req.params.heroId
+    SuperHero.findById({_id: heroId}, (err, hero) => {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        res.json({ msg: `Found: ${heroId}`, data: hero })
+      }
+    })
+  })
 
+Router.route('/api/superheroes/:heroId')
+  .delete((req, res) => {
+    const heroId = req.params.heroId
+    SuperHero.remove({_id: heroId}, (err, hero) => {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        res.json({ msg: `Deleted: ${hero}` })
+      }
+    })
+  })
 
-  module.exports = Router
+module.exports = Router
--- server/server.js
+++ server/server.js
@@ -2,33 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
+
 const heroRoutes = require('./routes/heroes')
 
 const port = 3001
 app.set('trust proxy', '127.0.0.1')
 
-mongoose.connect('mongodb://localhost/__superhero_DB__')
+mongoose.connect('mongodb://localhost/SUPERHEROES_DB')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./config/error-handler'))
 
-
-app.delete('/api/superheroes/:heroId', (req, res) => {
-  const heroId = req.params.heroId
-  SuperHero.remove({ _id: heroId }, (err, hero) => {
-    if (err) {
-      res.json({ msg: err })
-    } else {
-      res.json({ msg: 'Hero deleted!', data: {} })
-    }
-  })
-})
-
 app.use('/', heroRoutes)
 
-const server = app.listen(port, () => console.log(` 🎾 Running on port: ${port} 🎾 `))
-
-
+const server = app.listen(port, () => console.log(`Running on port: ${port}🔥`))
 
 module.exports = server
